405e99ea8eb971298c1666ad72d45d1f774a3e04
Marcnuth <hxianxian@qq.com>
2019-08-19 15:13:38 +0800

    add sentencizer

--- deduplication/nlp.py
+++ deduplication/nlp.py
@@ -6,13 +6,21 @@ BASIC_LAGNUAGE_MODEL = 'en_core_web_sm'
 DEFAULT_LANGUAGE_MODEL = 'xx_ent_wiki_sm'
 
 
-def tokenize(text, language_model=DEFAULT_LANGUAGE_MODEL, lemmatize=True, remove_stopwords=True, custom_filter=lambda x: x):
+def tokenize(text, language_model=DEFAULT_LANGUAGE_MODEL, lemmatize=True, remove_stopwords=True):
     spacy.load(BASIC_LAGNUAGE_MODEL)
     nlp = spacy.load(language_model)
     
     is_stopword = lambda x: remove_stopwords and x.norm_ in spacy.lang.en.stop_words.STOP_WORDS
-    is_removable = lambda x: is_stopword(x) and custom_filter(x.text) and x.is_punct and x.is_digit
+    is_useless = lambda x: x.is_punct or x.is_digit or x.is_space or x.like_num or x.like_url or x.like_email
+    is_removable = lambda x: is_stopword(x) or is_useless(x)
 
     token_value = lambda x: x.lemma_ if lemmatize else x.text
     yield from map(token_value, filterfalse(is_removable, nlp(text)))
 
+
+def sentencizer(text, language_model=DEFAULT_LANGUAGE_MODEL):
+    spacy.load(BASIC_LAGNUAGE_MODEL)
+    nlp = spacy.load(language_model)
+    nlp.add_pipe(nlp.create_pipe('sentencizer'))
+
+    yield from map(lambda x: x.text.strip(), nlp(text).sents)
--- tests/test_nlp.py
+++ tests/test_nlp.py
@@ -1,6 +1,18 @@
 from deduplication import nlp
+from pathlib import Path
 
 
 def test_main():
-    print('aaa')
-    pass
+    with open(Path(__file__).resolve().parent / 'data' / 'wiki_nlp.txt', 'r') as f:
+        content = f.read()
+
+    tokens = list(nlp.tokenize(content))
+    print(len(tokens), tokens)
+
+
+def test_sentences():
+    with open(Path(__file__).resolve().parent / 'data' / 'wiki_nlp.txt', 'r') as f:
+        content = f.read()
+
+    sents = list(nlp.sentencizer(content))
+    print(len(sents), sents)
